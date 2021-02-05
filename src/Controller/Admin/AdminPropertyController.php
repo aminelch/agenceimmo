81f147b89017d9e9ccd2bae45d1872fdf903b8e2
aminelch <aminelch95@gmail.com>
2021-02-05 23:32:09 +0100

    traduire le formulaire, créer l'action permet l'edition d'un bien

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -4,7 +4,9 @@
     use App\Entity\Property;
     use App\Form\PropertyType;
     use App\Repository\PropertyRepository;
+    use Doctrine\ORM\EntityManagerInterface;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +16,15 @@
          * @var PropertyRepository
          */
         private $repository;
+        /**
+         * @var EntityManagerInterface
+         */
+        private $em;
 
-        public function __construct(PropertyRepository $repository)
+        public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
         {
             $this->repository=$repository;
+            $this->em = $em;
         }
 
         /**
@@ -34,16 +41,24 @@
         }
 
         /**
-         * @Route("/admin/{id}", name="admin_property_edit")
+         * @Route("/admin/{id}", name="admin_property_edit", requirements={"id": "[0-9]*"})
          * @param Property $property
+         * @param Request  $request
          *
          * @return Response
          */
-        public function edit(Property $property):Response{
+        public function edit(Property $property, Request $request):Response{
             $form= $this->createForm(PropertyType::class, $property);
+            $form->handleRequest($request);
+            if($form->isSubmitted()){
+                $this->em->flush();
+//              dd('form submitted');
+                $this->addFlash('success', 'Le bien à été modifié');
+                return $this->redirectToRoute('admin_property_index');
+            }
 
             $current_menu='property';
-                return $this->render("admin/edit.html.twig",[
+            return $this->render("admin/edit.html.twig",[
                     'property'=>$property,
                     'current_menu'=>$current_menu,
                     'form'=>$form->createView()
--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Property;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +14,25 @@ class PropertyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description')
             ->add('title')
+            ->add('description', TextareaType::class,[
+                "label"=>"Description"
+            ])
             ->add('surface')
-            ->add('rooms')
-            ->add('bedrooms')
-            ->add('floor')
-            ->add('heat')
-            ->add('city')
-            ->add('address')
-            ->add('postal_code')
-            ->add('sold')
-            ->add('created_at')
-            ->add('price')
+            ->add('rooms',null,['label'=>'Pièces'])
+            ->add('bedrooms',null,['label'=>'Pièces'])
+            ->add('floor',null,['label'=>'floor'])
+            ->add('heat',ChoiceType::class, [
+                'label'=>'heat',
+                'choices'=>Property::HEAT
+//                'choices'=>$this->getChoices()
+            ])
+            ->add('city',null,['label'=>'Ville'])
+            ->add('address',null,['label'=>'Adresse'])
+            ->add('postal_code',null,['label'=>'Code postal'])
+            ->add('sold',null,['label'=>'Vendu?'])
+//            ->add('created_at')
+            ->add('price',null,['label'=>'Prix'])
         ;
     }
 
@@ -32,6 +40,17 @@ class PropertyType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Property::class,
+            'translation_domain'=>'forms'
         ]);
+    }
+
+    private function getChoices(): array
+    {
+        $choices = Property::HEAT;
+        $output=[];
+        foreach ($choices as $key =>$value){
+            $output[$value]=$key;
+        }
+        return $output;
     }
 }
